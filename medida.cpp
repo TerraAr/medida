02cabf630b68b7747263763815a870045441d385
TerraAr <fernando.carames@gmail.com>
2018-05-12 19:30:58 -0300

    Sobrecarga de operadores.  *Foram sobrecarregados os operadores +, -, *, /, =, +=, -=, *=, /=.

--- medida.cpp
+++ medida.cpp
@@ -1,8 +1,7 @@
 #include<cmath>
-#include<iostream>
+#include<stdio.h>
 
-#define PI 3.14159265358979323846
-#define complexo ponto2d
+#define PI M_PI
 
 class medida{
 private:
@@ -13,6 +12,9 @@ friend medida operator+(const medida&,const medida&);
 friend medida operator-(const medida&,const medida&);
 friend medida operator*(const medida&,const medida&);
 friend medida operator/(const medida&,const medida&);
+
+friend medida operator++(medida&);
+friend medida operator--(medida&);
 friend medida operator-(const medida&);
 
 friend medida operator=(medida&,const medida&);
@@ -21,7 +23,31 @@ friend medida operator-=(medida&,const medida&);
 friend medida operator*=(medida&,const medida&);
 friend medida operator/=(medida&,const medida&);
 
+template <class T> friend medida operator+(const medida&,const T&);
+template <class T> friend medida operator-(const medida&,const T&);
+template <class T> friend medida operator*(const medida&,const T&);
+template <class T> friend medida operator/(const medida&,const T&);
+
+template <class T> friend medida operator+(const T&,const medida&);
+template <class T> friend medida operator-(const T&,const medida&);
+template <class T> friend medida operator*(const T&,const medida&);
+template <class T> friend medida operator/(const T&,const medida&);
+
+template <class T> friend medida operator=(medida&,const T&);
+template <class T> friend medida operator+=(medida&,const T&);
+template <class T> friend medida operator-=(medida&,const T&);
+template <class T> friend medida operator*=(medida&,const T&);
+template <class T> friend medida operator/=(medida&,const T&);
+
+template <class T> friend T operator=(T&,const medida&);
+template <class T> friend T operator+=(T&,const medida&);
+template <class T> friend T operator-=(T&,const medida&);
+template <class T> friend T operator*=(T&,const medida&);
+template <class T> friend T operator/=(T&,const medida&);
+
 friend medida pow(const medida&,const double);
+friend medida sqrt(const medida&);
+friend medida cbrt(const medida&);
 
 medida(){
 valor[0]=0;
@@ -34,7 +60,7 @@ valor[1]=b;
 }
 
 void imprime(){
-std::cout<<valor[0]<<"±"<<valor[1]<<std::endl;
+printf("%lf±%lf\n",valor[0],valor[1]);
 }
 };
 
@@ -70,7 +96,18 @@ aux.valor[1]=aux.valor[0]-aux.valor[1];
 return aux;
 }
 
-medida operator-(medida& a){
+
+medida operator++(medida& a){
+a.medida[0]++
+return a;
+}
+
+medida operator--(medida& a){
+a.medida[0]--;
+return a;
+}
+
+medida operator-(const medida& a){
 medida aux;
 aux.valor[0]=-a.valor[0];
 aux.valor[1]=a.valor[1];
@@ -113,9 +150,138 @@ return a;
 }
 
 
+template <class T> medida operator+(const medida& a,const T& b){
+medida aux;
+aux.valor[0]=a.valor[0]+b;
+aux.valor[1]=a.valor[1];
+return aux;
+}
+
+template <class T> medida operator-(const medida& a,const T& b){
+medida aux;
+aux.valor[0]=a.valor[0]-b;
+aux.valor[1]=a.valor[1];
+return aux;
+}
+
+template <class T> medida operator*(const medida& a,const T& b){
+medida aux;
+aux.valor[0]=a.valor[0]*b;
+aux.valor[1]=a.valor[1];
+return aux;
+}
+
+template <class T> medida operator/(const medida& a,const T& b){
+medida aux;
+aux.valor[0]=a.valor[0]/b;
+aux.valor[1]=a.valor[1];
+return aux;
+}
+
+
+template <class T> medida operator+(const T& a,const medida& b){
+medida aux;
+aux.valor[0]=b.valor[0]+a;
+aux.valor[1]=b.valor[1];
+return aux;
+}
+
+template <class T> medida operator-(const T& a,const medida& b){
+medida aux;
+aux.valor[0]=b.valor[0]-a;
+aux.valor[1]=b.valor[1];
+return aux;
+}
+
+template <class T> medida operator*(const T& a,const medida& b){
+medida aux;
+aux.valor[0]=b.valor[0]*a;
+aux.valor[1]=b.valor[1];
+return aux;
+}
+
+template <class T> medida operator/(const T& a,const medida& b){
+medida aux;
+aux.valor[0]=b.valor[0]/a;
+aux.valor[1]=b.valor[1];
+return aux;
+}
+
+
+template <class T> medida operator=(medida& a,const T& b){
+a.valor[0]=b;
+a.valor[1]=0;
+return a;
+}
+
+template <class T> medida operator+=(medida& a,const T& b){
+a.valor[0]+=b;
+return a;
+}
+
+template <class T> medida operator-=(medida& a,const T& b){
+a.valor[0]-=b;
+return a;
+}
+
+template <class T> medida operator*=(medida& a,const T& b){
+a.valor[0]*=b;
+return a;
+}
+
+template <class T> medida operator/=(medida& a,const T& b){
+a.valor[0]/=b;
+return a;
+}
+
+
+template <class T> T operator=(T& a,const medida& b){
+a=b.valor[0];
+return a;
+}
+
+template <class T> T operator+=(T& a,const medida& b){
+a+=b.valor[0];
+return a;
+}
+
+template <class T> T operator-=(T& a,const medida& b){
+a-=b.valor[0];
+return a;
+}
+
+template <class T> T operator*=(T& a,const medida& b){
+a*=b.valor[0];
+return a;
+}
+
+template <class T> T operator/=(T& a,const medida& b){
+a/=b.valor[0];
+return a;
+}
+
+
 medida pow(const medida& a,const double x){
 medida aux;
 aux.valor[0]=pow(a.valor[0],x);
 aux.valor[1]=pow(a.valor[1],x);
+return aux;
+}
+
+medida sqrt(const medida& a){
+medida aux;
+aux.valor[0]=sqrt(a.valor[0]+a.valor[1]);
+aux.valor[1]=sqrt(a.valor[0]1a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=aux.valor[0]-aux.valor[1];
+return aux;
+}
+
+medida cbrt(const medida& a){
+medida aux;
+aux.valor[0]=cbrt(a.valor[0]+a.valor[1]);
+aux.valor[1]=cbrt(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=aux.valor[0]-aux.valor[1];
 return aux;
 }
