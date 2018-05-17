0872ff7d938380f3eb34aaf676bb8d14c0cf3bee
TerraAr <fernando.carames@gmail.com>
2018-05-17 17:03:53 -0300

    Adicionadas funções amigas.  *Adicionadas as funções trigonométricas diretas e seus arcos.

--- medida.cpp
+++ medida.cpp
@@ -49,6 +49,14 @@ friend medida pow(const medida&,const double);
 friend medida sqrt(const medida&);
 friend medida cbrt(const medida&);
 
+friend medida sin(const medida&);
+friend medida cos(const medida&);
+friend medida tan(const medida&);
+
+friend medida asin(const medida&);
+friend medida acos(const medida&);
+friend medida atan(const medida&);
+
 medida(){
 valor[0]=0;
 valor[1]=0;
@@ -281,6 +289,62 @@ medida cbrt(const medida& a){
 medida aux;
 aux.valor[0]=cbrt(a.valor[0]+a.valor[1]);
 aux.valor[1]=cbrt(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=aux.valor[0]-aux.valor[1];
+return aux;
+}
+
+medida sin(const medida& a){
+medida aux;
+aux.valor[0]=sin(a.valor[0]+a.valor[1]);
+aux.valor[1]=sin(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=abs(aux.valor[0]-aux.valor[1]);
+return aux;
+}
+
+medida cos(const medida& a){
+medida aux;
+aux.valor[0]=cos(a.valor[0]+a.valor[1]);
+aux.valor[1]=cos(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=abs(aux.valor[0]-aux.valor[1]);
+return aux;
+}
+
+medida tan(const medida& a){
+if(abs(a.valor[0]-PI)<a.valor[1]) return 0;
+medida aux;
+aux.valor[0]=tan(a.valor[0]+a.valor[1]);
+aux.valor[1]=tan(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=abs(aux.valor[0]-aux.valor[1]);
+return aux;
+}
+
+
+medida asin(const medida& a){
+medida aux;
+aux.valor[0]=asin(a.valor[0]+a.valor[1]);
+aux.valor[1]=asin(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=abs(aux.valor[0]-aux.valor[1]);
+return aux;
+}
+
+medida acos(const medida& a){
+medida aux;
+aux.valor[0]=acos(a.valor[0]+a.valor[1]);
+aux.valor[1]=acos(a.valor[0]-a.valor[1]);
+aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
+aux.valor[1]=abs(aux.valor[0]-aux.valor[1]);
+return aux;
+}
+
+medida atan(const medida& a){
+medida aux;
+aux.valor[0]=atan(a.valor[0]+a.valor[1]);
+aux.valor[1]=atan(a.valor[0]-a.valor[1]);
 aux.valor[0]=(aux.valor[0]+aux.valor[1])/2;
 aux.valor[1]=aux.valor[0]-aux.valor[1];
 return aux;
